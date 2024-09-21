0de59ef9e2aa8197b2bb4f281a9a47178d9ac7db
peachpranay <pranay.rajvanshii@gmail.com>
2024-09-21 11:21:07 +0530

    yes

--- backend/model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class  Todo(BaseModel):
-    title: str
-    description: str 
-
-class TodoUpdate(BaseModel):
-    description: str
--- backend/trial.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from database import create_a_todo  # Adjust the import if needed, depending on your directory structure.
-
-# Create a todo item
-todo_item = {"title": "Test Todo", "description": "This is a test item"}
-
-# Insert the todo item using the create_a_todo function
-asyncio.run(create_a_todo(todo_item))
-print("Todo item inserted successfully!")
